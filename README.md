5ba3ffda0c8b2a7dc0689cdbe2f51104bd6a2624
Simon Swanson <nomiswanson@gmail.com>
2014-01-18 21:19:20 -0600

    Some updates
    School getting in the way of serious work. Still optimizing the
    BeautifulSoup parsing, hopefully it’ll work out in the end.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-baseball-reference.stats
+thebaseballcubeScraper.py
+========================
+
+Extracts stats from thebaseballcube.com for each active player's stats. The choice to switch to thebaseballcube from baseball-reference was their inclusion of holds and quality starts, a statistic in many fantasy leagues.
+
+Future functionality should include the ability to search for college and minor league statistics, but those are currently in the works and may take a while to work with, since the priority is to retrieve stats for major league players. The data is stored in an XML file for future use, though I'm not sure if XML is the format I should be using for this.
+
+Makes use of the BeautifulSoup module to both extract information from the HTML files and to generate the XML. Again, I'm sure there's a more efficient way to do this, but that's what I'm working with for now.
+
+
+baseball-reference.Extraction.py
 ========================
 
 Extracts stats from baseball-reference.com of each active player's major league stats.
--- thebaseballcubeScraper.py
+++ thebaseballcubeScraper.py
@@ -5,14 +5,18 @@
 # takes in stats from thebaseballcube.com and exports to an XML file for future use
 
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import re
 from requests import get
 import sys
 
 
+# players that throw an error are saved in a dictionary to be spit out later
+warningPlayers = {}
+
+
 def argParse(args, leagues):
-	'''Takes in system args and turns them into a list of useable strings.'''
+	'''Takes in system args and turns them into a list of useable strings or errors out.'''
 
 	# default value with no arguments is to scrape mlb player data
 	if not args:
@@ -26,12 +30,12 @@ def argParse(args, leagues):
 		elif arg in leagueDict.keys():
 			leagues.append(leagueDict[arg])
 		else:
-			print('ERROR: "' + arg + '" is not a valid argument.')
+			print('ERROR: "{}" is not a valid argument.'.format(arg))
 			print('\nValid arguments:')
 			print('  "mlb" or "majors" for major league information')
 			print('  "milb" or "minors" for minor league information')
-			print('  "ncaa" or "college" for college player information')
-			print('\n  EXAMPLE: "' + sys.argv[0] + ' minors ncaa" retrieves data on active players that have not made it to the major leagues and all current college players.')
+			print('  "ncaa" or "college" for college player information - feature in progress')
+			print('\n  EXAMPLE: "{} minors majors" retrieves data on active players that have not made it to the major leagues and all active players that have made it to the major leagues.'.format(sys.argv[0]))
 			sys.exit(1)
 
 	return leagues
@@ -46,8 +50,8 @@ def indexParse(league):
 	if league == 'mlb':
 		links = soup.select('.letters a[href^="mlb"]')
 	else:
-		# currently only the > operator works because links for college are not working on the site right now
-		links = soup.select('.otherPlayers > a[href^=' + league + ']')
+		# currently the > operator works because links for college are not working on the site right now
+		links = soup.select('.otherPlayers > a[href^={}]'.format(league))
 
 	return links
 
@@ -56,20 +60,102 @@ def letterParse(links, profiles):
 	'''Takes in a list of links to letters and opens them for each non-retired player.'''
 	
 	for link in links:
-		soup = BeautifulSoup(get('mlb.asp?L=a').text)
-		# soup = BeautifulSoup(get('http://www.thebaseballcube.com/players/' + link.get('href')).text)
+		# soup = BeautifulSoup(get('mlb.asp?L=a').text)
+		soup = BeautifulSoup(get('http://www.thebaseballcube.com/players/{}'.format(link.get('href'))).text)
 		# all players are under the pre tag, active players are distinguished by including the b tag
 		# retrieves all links that lead to an active player's profile
-		for profile in soup.select('pre b a[href^="profile"]'):
-			profiles.append(profile)
+		profiles.append(profile) for profile in soup.select('pre b a[href^="profile"]'):
 
 	return profiles
 
 
 def profileParse(profiles):
-	'''Takes in a list of links to profiles and write out all player information to file.'''
+	'''Takes in a list of links to profiles and turns all needed information to an XML format.'''
+	
+	# initializes the XML
+	xml = BeautifulSoup(features='xml')
+	xml.append(xml.new_tag('players'))
 
+	# updates the XML for each player
+	for profile in profiles[:1]:
+		soup = BeautifulSoup(get('http://thebaseballcube.com/players/{}'.format(profile.get('href'))).text)
+		xml = playerInfo(xml, soup)
+
+	return xml
+
+
+def playerInfo(xml, soup):
+	'''Finds all basic information on the player and adds it to an XML document.'''
+
+	# finds the playe's personal information that will be used to identiy then later on
+	personalInfo = {}
+	personalInfo['FullName'] = infoSearch(soup, 'Full Name')
+	personalInfo['PlayerID'] = infoSearch(soup, 'TBC Player ID')
+	personalInfo['Birthday'] = infoSearch(soup, 'Born', 'a')
+	# the available attribute is set to yes when the player is created, will come up with a way to modify the attribute as I figure out how to work this during the season
+	personalInfo['Available'] = 'Yes'
+
+	# the player's stats for his main position is in the first statsCategories1 div tag
+	# the string in the statsCategories1 div tag shows whether the player is primarily a pitcher or batter
+	mainPosition = soup.find('div', class_='statsCategories1').string
+
+	# if the player is a pitcher or batter, retrieves the correct stats and updates the xml
+	# otherwise throws a warning, stores the player's name and ID, and continues
+	if 'PITCHING' in mainPosition:
+		stats = pitcherParse(soup)
+		xml = pitcherXML(personalInfo, stats, xml)
+	
+	else if 'BATTING' in mainPosition:
+		stats = hitterParse(soup)
+		xml = batterParse(personalInfo, stats, xml)
+	
+	else:
+		playerName = personalInfo['FullName']
+		playerID = personalInfo['PlayerID']
+		warningPlayers[playerID] = playerName
+		print('{} caused a problem. Continuing.'.format(playerName))
+
+	return xml
+
+
+def infoSearch(soup, match, subtag = ''):
+	'''Retrieves basic player information from the TBC header based on the string (and tag) given.'''
+
+	# the information needed is in the div tag after the identifying div tag, as given by the matching string
+	tag = soup.find('div', class_='cat1', text=match).findNextSibling('div')
+	
+	# for birthdays there is an a tag to go through before the string can be extracted
+	if subtag:
+		tag = tag.find(subtag)
+
+	# after navigating to the correct location in the tree, extracts the string contained
+	info = tag.string.strip()
+
+	return info
+
+
+def pitcherParse(soup):
+	'''Takes in a pitcher profile and adds the appropriate information to a dictionary.'''
+
+
+	mainStats = soup.find('div', class_='statsCategories1').find_next('table')
+	extraStatLink = soup.find('a', href=re.compile(r'.+Page=ExtraP'), text='Extra')['href']
+	stats = {}
+
+	return stats
+
+
+def hitterParse(soup):
+	positions = positionParse(xml, soup)
+	'''Takes in a hitter profile and adds the appropriate information to a dictionary.'''
 	return 0
+
+
+def positionParse(soup):
+	'''Returns all positions that the player played the previous year and the number of games played at those positions in a dictionary.'''
+	positions = {}
+	return positions
+
 
 
 def main():
@@ -77,8 +163,12 @@ def main():
 	leagues = argParse(sys.argv[1:], [])
 	for league in leagues:
 		links = indexParse(league)
-		profiles = letterParse(links, [])
-	return 0
+		# currently just takes in all the "a" players
+		profiles = letterParse(links[:1], [])
+		data = profileParse(profiles)
+
+	for key in sorted(warningPlayers.keys()):
+		print("Player {} (TBCID {}) was not entered into the database.".format(warningPlayers[key], key))
 
 if __name__ == '__main__':
-	main()
+	main()/
