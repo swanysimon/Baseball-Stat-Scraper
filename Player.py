030716bc76d045d7be8c9b8d560f8cae0fb2a071
Simon Swanson <nomiswanson@gmail.com>
2016-09-06 22:16:04 -0700

    first steps to a real db

--- Player.py
+++ Player.py
@@ -12,28 +12,24 @@ __maintainer__  = "Simon Swanson"
 
 class Player(object):
 
-    def __init__(self, playerID, fullName, rosterName, birthday, retired):
-        self.playerID  = playerID
-        self.name      = rosterName
-        self.fullName  = fullName
-        self.birthday  = birthday
-        self.retired   = retired
-        self.seasons   = {}
+    def __init__(self, playerID, fullName, rosterName, birthday, debut, retired):
+        self.playerID    = playerID
+        self.name        = rosterName
+        self.fullName    = fullName
+        self.birthday    = birthday
+        self.debut       = debut
+        self.retired     = retired
+        self.seasons     = {}
+        self.rd2eligible = { "positions": [] }
 
-    def getAge(self):
-        return self.birthday
-
-    def getName(self):
-        return self.name
-
-    def getplayerID(self):
-        return self.playerID
-
-    def getStats(self):
-        return self.seasons
-
-    def isRetired(self):
-        return self.retired
+    def __str__(self):
+        return "Player(playerID='{pid}', rosterName='{rName}', fullName='{fName}', birthday='{dob}', retired={retired}, seasons={stats})".format(
+                pid=self.playerID,
+                rName=self.name,
+                fName=self.fullName,
+                dob=self.birthday,
+                retired=self.retired,
+                stats=self.seasons)
 
     def addStats(self, seasonDict, category):
         for season in seasonDict.keys():
@@ -42,16 +38,16 @@ class Player(object):
     def addStat(self, season, seasonStats, category):
         if category not in self.seasons.keys():
             self.seasons[category] = {}
-        if season not in self.seasons[category].keys():
-            self.seasons[category][season] = {}
+        if year not in self.seasons[category].keys():
+            self.seasons[category][year] = {}
         for stat in seasonStats.keys():
-            if stat in self.seasons[category][season].keys():
-                if self.seasons[category][season][stat] == seasonStats[stat]:
+            if stat in self.seasons[category][year].keys():
+                if self.seasons[category][year][stat] == seasonStats[stat]:
                     print("Replacing {season}'s {stat} value {oldValue} with {newValue}".format(season=season, 
                             stat=stat, 
-                            oldValue=self.seasons[category][season][stat],
+                            oldValue=self.seasons[category][year][stat],
                             newValue=seasonStats[stat]))
-            self.seasons[category][season][stat] = seasonStats[stat]
+            self.seasons[category][year][stat] = seasonStats[stat]
 
     def addPositions(self, positionDict):
         self.addStats(positionDict, "fielding")
@@ -61,17 +57,47 @@ class Player(object):
         positionDict[position] = gamesPlayed
         self.addStat(season, positionDict, "fielding")
 
-    def __str__(self):
-        return "Player(playerID='{playerID}', rosterName='{rosterName}', fullName='{fullName}', birthday='{birthday}', retired={retired}, positions={positions}, seasons={seasons})".format(
-                playerID=self.playerID,
-                rosterName=self.name,
-                fullName=self.fullName,
-                birthday=self.birthday,
-                retired=self.retired,
-                positions=self.positions,
-                seasons=self.seasons)
+    def clean(self):
+        allStats = {
+            "batting": ["1b", "2b", "3b", "bb", "cs", "g", "gdp", "hbp", "hr", "ibb", "pa", "r", "rbi", "sb", "sf", "sh", "so"],
+            "pitching": ["bb", "bk", "bs", "cg", "er", "g", "gs", "h", "hb", "hld", "hr", "ibb", "ip", "l", "qs", "sho", "so", "sv", "w", "wp"],
+            "fielding": ["SP", "RP", "C", "1B", "2B", "3B", "SS", "OF", "CF", "U"]
+        }
 
+        for category in self.seasons.keys():
+            for year in self.seasons[category].keys():
+                for level in self.seasons[category][year].keys():
+                    for stat in allStats[category]:
+                        if stat not in self.seasons[category][year][level].keys():
+                            self.seasons[category][year][level][stat] = 0
+
+        years = sorted(self.seasons["fielding"].keys()).reverse()
+        for year in years:
+            if "mlb" in self.seasons["fielding"][year].keys():
+                for position in self.seasons["fielding"][year]["mlb"]:
+                    games    = 5 if position == "SP" else 10
+                    eligible = self.seasons["fielding"][year]["mlb"][position] >= games
+                    if eligible: self.rd2eligible["positions"].append(position)
+                if self.rd2eligible["positions"]: break
+
+        dbEntry = [
+            "br_player_info": (
+                    self.playerID,
+                    self.fullName,
+                    self.rosterName,
+                    self.birthday,
+                    self.retired,
+                    None
+            ),
+            "br_player_index": (
+
+            )
+            "pitching": [],
+            "fielding": [],
+            "eligibility": []
+        ]
 
 def version():
+    '''Print module version number'''
     print("Player ({version})".format(version=__version__))
 
